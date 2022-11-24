be245fe6f17c92bc8857cfa39cc03377bbe13f9c
Sami Männistö <sami.mannisto1@gmail.com>
2022-11-24 14:02:12 +0200

    Test correct ostos, TDD ex9

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -46,3 +46,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertAlmostEqual(len(ostokset), 1)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_tuote_on_oikea_ja_sita_on_oikea_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertAlmostEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertAlmostEqual(ostos.lukumaara(), 1)
