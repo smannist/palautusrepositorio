dbc96407c5810dbbd6c7b4ed49c286315d539217
Sami Männistö <sami.mannisto1@gmail.com>
2022-11-24 12:45:38 +0200

    Adding ostos to ostoskori minimal code, TDD ex2

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -17,8 +17,8 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        ostos = Ostos(lisattava)
+        self.ostoskori.append(ostos)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
